284fa5b7962099e6a9e2cfbb8e3631dcd63b5b67
danmarauda <143896437+danmarauda@users.noreply.github.com>
2024-04-26 05:28:45 +1000

    f

--- src/middleware.ts
+++ src/middleware.ts
@@ -20,8 +20,4 @@ export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 }
 
-// https://www.googleapis.com/auth/userinfo.email
-// https://www.googleapis.com/auth/userinfo.profile
-// https://www.googleapis.com/auth/drive.activity.readonly
-// https://www.googleapis.com/auth/drive.metadata
-// https://www.googleapis.com/auth/drive.readonly
+
